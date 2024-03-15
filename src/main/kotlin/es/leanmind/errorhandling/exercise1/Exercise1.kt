fa2658a792371e1c3450683a502306cf58d1326d
Jorge Aguiar Martin <jorge@leanmind.es>
2024-03-15 10:51:16 +0000

    exercise 1 implementation

---
+++ src/main/kotlin/es/leanmind/errorhandling/exercise1/Exercise1.kt
@@ -0,0 +1,42 @@
+package es.leanmind.errorhandling.exercise1
+
+interface Value {
+    fun value(): Any
+}
+
+class NumberValue(
+        private val value: Int
+): Value {
+    override fun value(): Int = value
+}
+
+class StringValue(
+        private val value: String
+): Value {
+    override fun value(): Any = value
+}
+
+class ValueList<T : Value>(
+        private val values: MutableList<T>
+) {
+    fun add(value: T) {
+        values.add(value)
+    }
+
+    fun filter(predicate: (T) -> Boolean): ValueList<T> {
+        return ValueList(values.filter(predicate).toMutableList())
+    }
+
+    override fun toString(): String {
+        return values.joinToString(", ") { it.value().toString() }
+    }
+}
+
+/*
+
+public override string ToString()
+{
+    return String.Join(", ", values.Select(value => value.GetValue()));
+}
+
+*/
---
+++ src/test/kotlin/es/leanmind/errorhandling/exercise1/Exercise1Should.kt
@@ -0,0 +1,26 @@
+package es.leanmind.errorhandling.exercise1
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Exercise1Should {
+    @Test
+    fun work() {
+        val numberValue1 = NumberValue(1)
+        val numberValue2 = NumberValue(2)
+
+        val stringValue1 = StringValue("hello")
+        val stringValue2 = StringValue("world")
+
+        val values = ValueList(mutableListOf(numberValue1, numberValue2, stringValue1, stringValue2))
+
+        values.add(StringValue("foo"))
+
+        val numberValues = values.filter { it is NumberValue }
+        val stringValues = values.filter { it is StringValue }
+
+        assertThat(values.toString()).isEqualTo("1, 2, hello, world, foo")
+        assertThat(numberValues.toString()).isEqualTo("1, 2")
+        assertThat(stringValues.toString()).isEqualTo("hello, world, foo")
+    }
+}
